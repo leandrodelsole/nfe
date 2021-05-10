d56de290353c6a4408c5b1521486996af8db9fda
Leandro Del Sole <leandrodelsole@gmail.com>
2021-05-10 19:28:50 -0300

    nt nova

--- src/main/java/com/fincatto/documentofiscal/validadores/DFXMLValidador.java
+++ src/main/java/com/fincatto/documentofiscal/validadores/DFXMLValidador.java
@@ -1,14 +1,15 @@
 package com.fincatto.documentofiscal.validadores;
 
-import org.xml.sax.SAXException;
-
-import javax.xml.transform.stream.StreamSource;
-import javax.xml.validation.Schema;
-import javax.xml.validation.SchemaFactory;
 import java.io.IOException;
 import java.io.StringReader;
 import java.net.URISyntaxException;
 import java.net.URL;
+
+import javax.xml.transform.stream.StreamSource;
+import javax.xml.validation.Schema;
+import javax.xml.validation.SchemaFactory;
+
+import org.xml.sax.SAXException;
 
 public final class DFXMLValidador {
 
@@ -29,7 +30,7 @@ public final class DFXMLValidador {
     }
 
     public static boolean valida400(final String xml, final String xsd) throws IOException, SAXException, URISyntaxException {
-        final URL xsdPath = DFXMLValidador.class.getClassLoader().getResource(String.format("schemas/PL_009_V4_00_NT_2020_006_v1.00/%s", xsd));
+        final URL xsdPath = DFXMLValidador.class.getClassLoader().getResource(String.format("schemas/PL_009_V4_00_NT_2020_006_v1.20/%s", xsd));
         final SchemaFactory schemaFactory = SchemaFactory.newInstance("http://www.w3.org/2001/XMLSchema");
         final Schema schema = schemaFactory.newSchema(new StreamSource(xsdPath.toURI().toString()));
         schema.newValidator().validate(new StreamSource(new StringReader(xml)));
